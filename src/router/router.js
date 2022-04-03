7eeda0da3ef3b6b912dbf97166bafcd093f04e0b
Juanma Cano <juanmacano@gmail.com>
2022-04-03 09:38:46 +0200

    rutas - lazyload

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,29 @@
-import {createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHashHistory } from "vue-router";
 
-import AboutPage from '../modules/pokemon/pages/AboutPage.vue';
-import ListPage from '../modules/pokemon/pages/ListPage.vue';
-import PokemonPage from '../modules/pokemon/pages/PokemonPage.vue';
-import NotFoundPage from '../modules/shared/pages/NotFoundPage.vue';
+// carga todo al comienzo
+/* import AboutPage from '../modules/pokemon/pages/AboutPage.vue';
+import ListPage from "../modules/pokemon/pages/ListPage.vue";
+import PokemonPage from "../modules/pokemon/pages/PokemonPage.vue";
+import NotFoundPage from "../modules/shared/pages/NotFoundPage.vue"; */
 
+// carga con lazyload, se hace la importación del módulo a demanda
 const routes = [
-	{path: '/', component: ListPage},
-	{path: '/about', component: AboutPage},
-	{path: '/id', component: PokemonPage},
-	{path: '/:pathMath(.*)*', component: NotFoundPage}, // error 404
+	{
+		path: "/",
+		component: () => import("../modules/pokemon/pages/ListPage.vue"),
+	},
+	{
+		path: "/about",
+		component: () => import("../modules/pokemon/pages/AboutPage.vue"),
+	},
+	{
+		path: "/id",
+		component: () => import("../modules/pokemon/pages/PokemonPage.vue"),
+	},
+	{
+		path: "/:pathMath(.*)*", // error 404
+		component: () => import("../modules/shared/pages/NotFoundPage.vue"),
+	},
 ];
 
 const router = createRouter({
